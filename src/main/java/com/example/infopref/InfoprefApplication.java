c89e7ecb6a5d5871bec42a9af2e47ff4caeac3e5
LeoMulinari <leo.mulinari@gmail.com>
2024-08-01 18:33:39 -0300

    configuração inicial

---
+++ src/main/java/com/example/infopref/InfoprefApplication.java
@@ -0,0 +1,13 @@
+package com.example.infopref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoprefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoprefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infopref/InfoprefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infopref;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoprefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
