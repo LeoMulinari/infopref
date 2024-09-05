cb3b3bc8903a64771b98602a136879d18d20a7a3
LeoMulinari <leo.mulinari@gmail.com>
2024-09-05 02:11:52 -0300

    UserDetailsService e JWTUtil criados

--- src/main/java/com/example/infopref/repositories/UserRepository.java
+++ src/main/java/com/example/infopref/repositories/UserRepository.java
@@ -2,10 +2,13 @@ package com.example.infopref.repositories;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.infopref.models.User;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    @Transactional(readOnly = true)
+    User findByUsername(String username);
 }
